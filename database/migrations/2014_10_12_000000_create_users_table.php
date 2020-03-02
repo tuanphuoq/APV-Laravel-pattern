db2c18ce9eff59975fa52951e7a71f4c22cc2442
Tuan TT <tuantt2429x@gmail.com>
2020-03-02 08:56:15 +0700

    fix users migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,12 +17,10 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('password');
-            $table->integer('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->text('gender');
             $table->date('dob');
-
             $table->rememberToken();
             $table->timestamps();
         });
